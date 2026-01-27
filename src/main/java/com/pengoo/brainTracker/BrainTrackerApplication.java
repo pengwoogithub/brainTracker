aff3163c34414e0ececf2b67a5ba35e3a23c46bb
Dox <148692807+pengwoogithub@users.noreply.github.com>
2026-01-27 18:19:55 +0800

    project start - initial commit

---
+++ src/main/java/com/pengoo/brainTracker/BrainTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.pengoo.brainTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainTrackerApplication.class, args);
+	}
+
+}
